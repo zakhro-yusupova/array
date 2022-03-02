52069a38ed51e2f4854243e1a5c6a308e5189905
zakhro-yusupova <yu.zakhro01@gmail.com>
2022-03-02 23:36:19 +0500

    Remove background color from result

--- js/main.js
+++ js/main.js
@@ -7,25 +7,20 @@ elForm.addEventListener("submit", function(evt){
   evt.preventDefault();
 
   var elInputVal = elFormInput.value;
+  var oddNumber =[11 , 33, 55,];
+  var evenNumbers = [12 , 34 , 56 ,];
 
   if(elInputVal % 2 == 0 ){
 
     elFormResult.textContent = "Juft Son";
     elFormResult.classList.add("result-success");
-    even = evenNumbers.push(elInputVal);
+    evenNumbers.push(elInputVal);
     console.log(evenNumbers);
-  }
-
-  else{
+  } else{
     elFormResult.textContent= "Toq son";
     elFormResult.classList.add("result-error");
-    odd = oddNumber.push(elInputVal);
+    oddNumber.push(elInputVal);
     console.log(oddNumber);
   }
 
-  elInput.value = null;
-
 })
-
-var oddNumber =[1 , 3, 5, 7, 9 ,];
-var evenNumbers = [2 , 4 , 6 , 8 , 10 ,];
